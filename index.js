4d53cc83a35817c7ab33ab9466f22f19414eb924
KenSa0day <108104817+Kensa0day@users.noreply.github.com>
2023-10-15 12:30:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ connectDB();
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+})
+
 //rute
 app.get('/', (req, res) => {
     res.send('Hello World!')
